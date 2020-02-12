3320df37abcbde0167bb1a928ff0a1b96da68b15
Glovecc <664037691@qq.com>
2020-02-12 17:45:55 +0800

    commits

---
+++ README.md
@@ -0,0 +1,27 @@
+# ulauncher-github-search
+
+[![Build Status](https://img.shields.io/travis/com/brpaz/ulauncher-devdocs.svg)](https://github.com/brpaz/ulauncher-devdocs)
+[![GitHub license](https://img.shields.io/github/license/brpaz/ulauncher-devdocs.svg)](https://github.com/brpaz/ulauncher-devdocs/blob/master/LICENSE)
+
+> [Ulauncher](https://ulauncher.io) extension to search repository on Github
+
+## Demo
+
+![demo.png](demo.png)
+
+## Requirements
+
+- Ulauncher >5
+- Python >= 3
+
+## Install
+
+Open ulauncher preferences window -> extensions -> add extension and paste the following url:
+
+```
+https://github.com/Glovecc/ulauncher-github-search
+```
+
+## License 
+
+Copywright @ 2020 [Junian](https://github.com/Glovecc)
---
+++ main.py
@@ -0,0 +1,45 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+import requests
+
+#Proxies = {"http": "http://127.0.0.1:7890", "https": "http://127.0.0.1:7890"}
+
+
+class Extension(Extension):
+
+    def __init__(self):
+        super(Extension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        query = event.get_argument() or str()
+        if len(query.strip()) == 0:
+            return RenderResultListAction([
+                ExtensionResultItem(icon='images/icon.png',
+                                    name='No input',
+                                    on_enter=HideWindowAction())
+            ])
+        else:
+            result = requests.get(
+                "https://api.github.com/search/repositories?q=%s&sort=stars&page=1&order=desc&per_page=10" % query, headers={"Authorization": "token 9ba0255c370e43b0fc17cb23406cad74d4bd0a5b"}).json()
+            items = []
+            for i in result["items"]:
+                items.append(ExtensionResultItem(icon='images/icon.png',
+                                                 name=i["full_name"],
+                                                 description=str(
+                                                     i["description"]),
+                                                 on_enter=OpenUrlAction(i["html_url"])))
+
+            return RenderResultListAction(items)
+
+
+if __name__ == '__main__':
+    Extension().run()
